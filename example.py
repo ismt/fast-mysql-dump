c649153eefbe8822854d7c6a5a77ee18ae9cb4c9
workism <stanislav.ivanov@trident.software>
2026-05-20 19:40:25 +0300

    ...

--- example.py
+++ example.py
@@ -1,43 +1,29 @@
 import lib_my
 
-
-# import winsound
-
-
-def test_site():
+def zen_api_prod(remote_mysql_dbname: str = 'prod_api2', local_mysql_dbname: str = 'zen__prod_api2'):
     obj = lib_my.CopyMysqlDbRemoteToLocal(
-        dump_name='zen_api_stag',
+        dump_name='zen__prod_api2',
 
-        remote_ssh_hostname='192.168.2.1',
+        remote_ssh_hostname='192.168.2.5',
         remote_ssh_username='dev_stem',
         remote_ssh_password='',
-        remote_ssh_port=53125,
+        remote_ssh_port=53130,
 
-        remote_ssh_key_filename=r'C:\Users\T\.ssh\testttt',
+        remote_ssh_key_filename=r'C:\Users\T\.ssh\key',
 
-        remote_mysql_hostname='ttt',
+        remote_mysql_hostname='ttt.tt.tt',
 
-        remote_mysql_dbname='stag_api',
+        remote_mysql_dbname=remote_mysql_dbname,
         remote_mysql_username='admin',
         remote_mysql_password='',
 
         remote_mysql_dump_compressor='zstd',
         remote_mysql_ignore_tables=[],
-        # local_mysql_dbname = 'zen__stag_api_test_logistic5',
-        local_mysql_dbname = 'zen__stag_api',
-        local_mysql_hostname = '192.168.2.87'
+        local_mysql_dbname=local_mysql_dbname,
+        local_mysql_hostname='192.168.2.87'
     )
 
     obj.connect()
-
     obj.dump_remote_and_download()
     obj.unpack()
-    # obj.change_row_format('dynamic')
-    # obj.remove_definer_from_file()
     obj.restore_local()
-    obj.clean_dump_files()
-
-
-test_site()
-
-# winsound.Beep(500, 3000)
